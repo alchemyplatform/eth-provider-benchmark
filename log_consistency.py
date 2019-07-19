d8103111ca40e604d3f06569716be3e7747af450
Omar Ceja <oti@Omars-MacBook-Pro.local>
2019-07-19 11:14:01 -0700

    changed console messages

--- log_consistency.py
+++ log_consistency.py
@@ -4,10 +4,8 @@ import provider_benchmark
 import config
 
 def logTest(web3, company):
-    repetitionInconsitency = 0
     methodInconsitency = 0
     getLogByHashFailure = 0
-    getLogByFromToFailure = 0
     latestBlockFailure = 0
     logsDic = {}
     
@@ -25,19 +23,6 @@ def logTest(web3, company):
                 getLogByHashFailure += 1
                 time.sleep(config.DELAY)
                 continue
-            try:
-                logByFromTo = web3.eth.getLogs({'fromBlock': latestBlock.number, 'toBlock': latestBlock.number})
-            except Exception as e:
-                if config.VERBOSE: print (company, "gave the following error when trying to find logs for the latest block", latestBlock.number, "by fromBlock and toBlock:", e)
-                getLogByFromToFailure += 1
-                time.sleep(config.DELAY)
-                continue
-
-            if logByHash != logByFromTo:
-                repetitionInconsitency += 1
-                if config.VERBOSE: print (company, "gave inconsistent logs for the latest block when queried by blockHash vs fromBlock and toBlock")
-                time.sleep(config.DELAY)
-                continue
             
             if latestBlockHash in logsDic:
                 if logsDic[latestBlockHash] != logByHash:
@@ -52,7 +37,5 @@ def logTest(web3, company):
         time.sleep(config.DELAY)
     
     print (company, "returned a different log for the same latest block:", methodInconsitency/config.ITERATIONS * 100, "%")
-    print (company, "returned a different log based on querying by blockHash vs fromBlock and toBlock:", repetitionInconsitency/config.ITERATIONS * 100, "%")
     print (company, "failed to return the latest block:", latestBlockFailure/config.ITERATIONS * 100, "%")
     print (company, "failed to getLogs on the latest block queried by blockHash:", getLogByHashFailure/config.ITERATIONS * 100, "%")
-    print (company, "failed to getLogs on the latest block queried by fromBlock and toBlock:", getLogByFromToFailure/config.ITERATIONS * 100, "%")
